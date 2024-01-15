c5aea75bac1512191e9fa286328523f388c78dfd
MrPokk <dr.mokrousoff2017@yandex.ru>
2024-01-15 17:16:09 +0300

    Version 0.1 Start commit

---
+++ Generator/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(75, 75, 75);
+            button1.Font = new Font("Segoe UI Variable Display Semib", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(68, 322);
+            button1.Name = "button1";
+            button1.Size = new Size(250, 46);
+            button1.TabIndex = 1;
+            button1.Text = "Загрузить";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(75, 75, 75);
+            button2.Font = new Font("Segoe UI Variable Display Semib", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(324, 322);
+            button2.Name = "button2";
+            button2.Size = new Size(250, 46);
+            button2.TabIndex = 2;
+            button2.Text = "Скачать";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(75, 75, 75);
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(68, 46);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(250, 250);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            pictureBox2.BackColor = Color.FromArgb(75, 75, 75);
+            pictureBox2.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox2.Location = new Point(324, 46);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(250, 250);
+            pictureBox2.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleMode = AutoScaleMode.Inherit;
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            BackColor = Color.FromArgb(32, 32, 32);
+            ClientSize = new Size(643, 450);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Padding = new Padding(16);
+            Text = "Generator Totem";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button button1;
+        private Button button2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
---
+++ Generator/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Numerics;
+using System.Runtime.Intrinsics.X86;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Generator
+{
+    public partial class Form1 : Form
+    {
+
+        public Bitmap _bitmapTotem;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.FlatAppearance.BorderSize = 0;
+
+
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.FlatAppearance.BorderSize = 0;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            OpenFileDialog FileData = new OpenFileDialog();
+
+            FileData.Filter = "Export PNG (*.png)|*.png";
+            if (FileData.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Bitmap image = new Bitmap(FileData.FileName);
+                    Bitmap totem = CreateBitmapTotem(image);
+
+                    _bitmapTotem = totem;
+
+                    pictureBox1.Image = ScaleBimaps(image, 4);
+                    pictureBox2.Image = ScaleBimaps(totem, 5);
+
+                }
+                catch { }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1 != null)
+            {
+                SaveFileDialog FileData = new SaveFileDialog();
+                FileData.Filter = "PNG (*.png)|*.png";
+                if (FileData.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+
+                        _bitmapTotem.Save(FileData.FileName, ImageFormat.Png);
+                    }
+                    catch { }
+                }
+            }
+        }
+
+        private Bitmap CreateBitmapTotem(Bitmap inputFile)
+        {
+
+
+            inputFile = new Bitmap(inputFile);
+            Bitmap outputFile = new Bitmap(16, 16);
+
+            Bitmap Head = CutBitmap(inputFile, 8, 8, new Point(3, 0), new Size(8, 8));
+            Bitmap Body = CutBitmap(inputFile, 20, 20, new Point(3, 8), new Size(8, 4));
+            Bitmap Leg = MergerBitmaps(CutBitmap(inputFile, 7, 28, new Point(4, 12), new Size(6, 2)), CutBitmap(inputFile, 8, 31, new Point(5, 14), new Size(4, 1)));
+
+            Bitmap HandRight = MergerBitmaps(CutBitmap(inputFile, 33, 52, new Point(11, 8), new Size(2, 3)), CutBitmap(inputFile, 40, 48, new Point(13, 8), new Size(1, 2)));
+            Bitmap HandLeft = MergerBitmaps(CutBitmap(inputFile, 33, 52, new Point(1, 8), new Size(2, 3)), CutBitmap(inputFile, 40, 48, new Point(0, 8), new Size(1, 2)));
+
+
+            List<Bitmap> allElement = new List<Bitmap>() { Head, Body, HandLeft, HandRight, Leg };
+
+            outputFile = MergerAllBitmaps(allElement, new Point(1, 1));
+
+
+            return outputFile;
+
+        }
+        // Увеличение Размера картинки 
+        private Bitmap ScaleBimaps(Bitmap inputBitmap, int resizeScale)
+        {
+            Bitmap scaleOutputFile = new Bitmap(inputBitmap.Width * resizeScale, inputBitmap.Height * resizeScale);
+            using (Graphics GraphicsPixel = Graphics.FromImage(scaleOutputFile))
+            {
+                GraphicsPixel.InterpolationMode = InterpolationMode.NearestNeighbor;
+                GraphicsPixel.DrawImage(inputBitmap, new Rectangle(0, 0, inputBitmap.Width * resizeScale, inputBitmap.Height * resizeScale));
+            }
+            return scaleOutputFile;
+        }
+        // Отризает часть от картинки
+        private Bitmap CutBitmap(Bitmap inputBitmap, int poseX, int poseY, Point offset, Size bitmapArea)
+        {
+            //16 константа размера картинки для удобства
+            Bitmap outputFile = new Bitmap(16, 16);
+
+            for (int y = 0; y < bitmapArea.Height; y++)
+            {
+                for (int x = 0; x < bitmapArea.Width; x++)
+                {
+                    Color pixelColor = inputBitmap.GetPixel(x + poseX, y + poseY);
+                    outputFile.SetPixel(x + offset.X, y + offset.Y, pixelColor);
+                }
+            }
+
+            return outputFile;
+        }
+        // Формируте только 2 картинки
+        private Bitmap MergerBitmaps(Bitmap BitmapOne, Bitmap BitmapTwo)
+        {
+            Bitmap result = new Bitmap(Math.Max(BitmapOne.Width, BitmapTwo.Width),
+                                       Math.Max(BitmapOne.Height, BitmapTwo.Height));
+            using (Graphics GraphicsPixel = Graphics.FromImage(result))
+            {
+                GraphicsPixel.DrawImage(BitmapOne, Point.Empty);
+                GraphicsPixel.DrawImage(BitmapTwo, Point.Empty);
+
+            }
+            return result;
+        }
+        // Формирует общие картинки
+        private Bitmap MergerAllBitmaps(List<Bitmap> allBitmap, Point offset)
+        {
+            int maxHeigh = allBitmap[0].Height;
+            int maxWidth = allBitmap[0].Width;
+            for (int i = 0; i < allBitmap.Count; i++)
+            {
+                if (maxHeigh < allBitmap[i].Height)
+                {
+                    maxHeigh = allBitmap[i].Height;
+                }
+                if (maxWidth < allBitmap[i].Width)
+                {
+                    maxWidth = allBitmap[i].Width;
+                }
+            }
+            Bitmap reasult = new Bitmap(maxWidth, maxHeigh);
+            using (Graphics GrahicsPixel = Graphics.FromImage(reasult))
+            {
+                for (int i = 0; i < allBitmap.Count; i++)
+                {
+                    GrahicsPixel.DrawImage(allBitmap[i], offset);
+                }
+            }
+            return reasult;
+        }
+
+    }
+}
